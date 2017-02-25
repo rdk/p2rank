2745d4fd2ebb93bf5f541e118ee735b0023c262e
rdk <rdk@users.noreply.github.com>
2017-02-26 00:29:43 +0100

    +testset

--- misc/test-scripts/testsets.sh
+++ misc/test-scripts/testsets.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+\
 ###################################################################################################################
 
 SET=$1
@@ -222,6 +222,14 @@ speed_quick() {
 
     misc/test-scripts/benchmark.sh 3  "U48"   "1 2 4 5 8 9 12 13 24"  "./prank.sh predict u48.ds -c workdef -out_subdir SPEED"
     misc/test-scripts/benchmark.sh 10 "1FILE" "1"                     "./prank.sh predict -f distro/test_data/liganated/1aaxa.pdb -c workdef -out_subdir SPEED"
+}
+
+speed_joined() {
+
+    title SPEED TESTS
+
+    misc/test-scripts/benchmark.sh 3  "U48"   "1 2 3 4 5 6 7 8 9 10 11 12 13"  "./prank.sh predict joined.ds -c workdef -out_subdir SPEED"
+    misc/test-scripts/benchmark.sh 10 "1FILE" "1"                              "./prank.sh predict -f distro/test_data/liganated/1aaxa.pdb -c workdef -out_subdir SPEED"
 }
 
 
