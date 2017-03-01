43b5ade50bcba3e65efe80a20a31f56bde1562c3
rdk <rdk@users.noreply.github.com>
2017-03-01 16:58:34 +0100

    R plot size

--- src/main/groovy/cz/siret/prank/utils/plotter/RPlotter.groovy
+++ src/main/groovy/cz/siret/prank/utils/plotter/RPlotter.groovy
@@ -133,27 +133,28 @@ class RPlotter implements Parametrized {
 
                 # creates a 5 x 5 inch image
                 png("${label}.png",    # create PNG for the heat map
-                    width = 5*300,        # 5 x 300 pixels
+                    width = 10*300,        # 5 x 300 pixels
                     height = 5*300,
                     res = 300,            # 300 pixels per inch
-                    pointsize = 6)        # smaller font size
+                    pointsize = 5)        # smaller font size
 
 
                 #par(cex.main=1.5) # increase ception font size
                 heatmap.2(mat_data,
+                          main = "$label",      # heat map title
                           cellnote = mat_data,  # same data set for cell labels
-                          main = "$label", # heat map title
                           notecol="black",      # change font color of cell labels to black
+                          notecex=0.5,          # cell note font size multiplier 
                           density.info="none",  # turns off density plot inside color legend
                           trace="none",         # turns off trace lines inside the heat map
-                          margins =c(8,8),     # widens margins around plot
+                          # margins =c(8,8),    # widens margins around plot
                           col=my_palette,       # use on color palette defined earlier
-                          #breaks=col_breaks,    # enable color transition at specified limits
-                          dendrogram="none",     # only draw a row dendrogram
+                          #breaks=col_breaks,   # enable color transition at specified limits
+                          dendrogram="none",    # only draw a row dendrogram
                           xlab="$xlab",
                           ylab="$ylab",
-                          Rowv=FALSE,            # turn off row clustering
-                          Colv=FALSE)            # turn off column clustering
+                          Rowv=FALSE,           # turn off row clustering
+                          Colv=FALSE)           # turn off column clustering
 
                 dev.off()               # close the PNG device
         """
