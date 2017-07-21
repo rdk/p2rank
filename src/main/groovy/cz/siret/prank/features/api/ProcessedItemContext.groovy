268e6ea4ef93ad04424063d934b80dcf3a9b84eb
rdk <rdk@users.noreply.github.com>
2017-07-21 16:09:37 +0200

    auxData in ProcessedItemContext

--- src/main/groovy/cz/siret/prank/features/api/ProcessedItemContext.groovy
+++ src/main/groovy/cz/siret/prank/features/api/ProcessedItemContext.groovy
@@ -5,7 +5,15 @@ package cz.siret.prank.features.api
  */
 class ProcessedItemContext {
 
+    /**
+     * Column falues from dataset item
+     */
     Map<String, String> datsetColumnValues
+
+    /**
+     * Generic store for passing any custom attributes or data
+     */
+    Map<String, Object> auxData = new HashMap<>()
 
     ProcessedItemContext(Map<String, String> datsetColumnValues) {
         this.datsetColumnValues = datsetColumnValues
