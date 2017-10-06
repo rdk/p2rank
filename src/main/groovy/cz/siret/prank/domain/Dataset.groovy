317c9937288587f4f203e1a28484932070abc94d
rdk <rdk@users.noreply.github.com>
2017-10-06 14:24:51 +0200

    deepsite loader

--- src/main/groovy/cz/siret/prank/domain/Dataset.groovy
+++ src/main/groovy/cz/siret/prank/domain/Dataset.groovy
@@ -3,6 +3,7 @@ package cz.siret.prank.domain
 import com.google.common.base.CharMatcher
 import com.google.common.base.Splitter
 import cz.siret.prank.domain.loaders.ConcavityLoader
+import cz.siret.prank.domain.loaders.DeepSiteLoader
 import cz.siret.prank.domain.loaders.FPockeLoader
 import cz.siret.prank.domain.loaders.LiseLoader
 import cz.siret.prank.domain.loaders.PredictionLoader
@@ -321,6 +322,9 @@ class Dataset implements Parametrized {
                 break
             case "lise":
                 res = new LiseLoader()
+                break
+            case "deepsite":
+                res = new DeepSiteLoader()
                 break
             default:
                 res = new FPockeLoader() // TODO: throw exception here, should not be run on prank predict
---
+++ src/main/groovy/cz/siret/prank/domain/loaders/DeepSiteLoader.groovy
@@ -0,0 +1,85 @@
+package cz.siret.prank.domain.loaders
+
+import cz.siret.prank.domain.Pocket
+import cz.siret.prank.domain.Prediction
+import cz.siret.prank.domain.Protein
+import cz.siret.prank.geom.Point
+import cz.siret.prank.utils.PDBUtils
+import cz.siret.prank.utils.StrUtils
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+import org.biojava.nbio.structure.Structure
+
+/**
+ * Loader for DeepSite pocket predictions from *_results.pdb file
+ */
+@Slf4j
+@CompileStatic
+class DeepSiteLoader extends PredictionLoader {
+
+    /**
+     * cutoff for determining surface atoms around centroid
+     */
+    double SURFACE_ATOMS_CUTOFF = 8
+
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    Prediction loadPrediction(String predictionOutputFile, Protein liganatedProtein) {
+
+        List<DeepSitePocket> pockets = loadSitehoundPockets(predictionOutputFile, liganatedProtein)
+
+        return new Prediction(liganatedProtein, pockets)
+    }
+
+    /**
+                                     x         y       z           score
+     HETATM 2770  O   HOH X   1      37.560  27.272  15.616  0.00  1.00      XXX
+     HETATM 2771  O   HOH X   2      45.560  21.272  35.616  0.00  0.99      XXX
+     HETATM 2772  O   HOH X   3      53.560  31.272  25.616  0.00  0.95      XXX
+     HETATM 2773  O   HOH X   4      47.560  25.272   5.616  0.00  0.98      XXX
+     HETATM 2774  O   HOH X   5      51.560  21.272   9.616  0.00  0.74      XXX
+     */
+    List<DeepSitePocket> loadSitehoundPockets(String predictionOutputFile, Protein liganatedProtein) {
+
+        List<DeepSitePocket> res = new ArrayList<>()
+
+        Structure struct = PDBUtils.loadFromFile(predictionOutputFile)
+
+        List<String> lines = new File(predictionOutputFile).text.trim().readLines().findAll { it.startsWith('HETATM') }.toList()
+
+        for (String line : lines) {
+
+            List<String> cols = StrUtils.splitOnWhitespace(line)
+
+            DeepSitePocket poc = new DeepSitePocket()
+
+            poc.rank = cols[4].toInteger() + 1
+            poc.name =  cols[3]
+            poc.score = cols[9].toDouble()
+
+            double x = cols[5].toDouble()
+            double y = cols[6].toDouble()
+            double z = cols[7].toDouble()
+
+            poc.centroid = new Point(x, y, z)
+            if (liganatedProtein!=null) {
+                poc.surfaceAtoms = liganatedProtein.exposedAtoms.cutoffAroundAtom(poc.centroid, SURFACE_ATOMS_CUTOFF)
+            }
+
+            res.add(poc)
+        }
+
+        return res
+    }
+
+    static class DeepSitePocket extends Pocket {
+
+        double score
+
+    }
+
+}
