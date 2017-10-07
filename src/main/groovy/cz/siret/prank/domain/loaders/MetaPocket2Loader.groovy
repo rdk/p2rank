110576bbf86f03d020b774225caca7f48a17fb1b
rdk <rdk@users.noreply.github.com>
2017-10-07 05:33:04 +0200

    metapocket loader fix

--- src/main/groovy/cz/siret/prank/domain/loaders/MetaPocket2Loader.groovy
+++ src/main/groovy/cz/siret/prank/domain/loaders/MetaPocket2Loader.groovy
@@ -36,18 +36,41 @@ class MetaPocket2Loader extends PredictionLoader {
     }
 
     /**
+     Record Format
+
+     COLUMNS        DATA  TYPE    FIELD        DEFINITION
+     -------------------------------------------------------------------------------------
+     1 -  6        Record name   "ATOM  "
+     7 - 11        Integer       serial       Atom  serial number.
+     13 - 16        Atom          name         Atom name.
+     17             Character     altLoc       Alternate location indicator.
+     18 - 20        Residue name  resName      Residue name.
+     22             Character     chainID      Chain identifier.
+     23 - 26        Integer       resSeq       Residue sequence number.
+     27             AChar         iCode        Code for insertion of residues.
+     31 - 38        Real(8.3)     x            Orthogonal coordinates for X in Angstroms.
+     39 - 46        Real(8.3)     y            Orthogonal coordinates for Y in Angstroms.
+     47 - 54        Real(8.3)     z            Orthogonal coordinates for Z in Angstroms.
+     55 - 60        Real(6.2)     occupancy    Occupancy.
+     61 - 66        Real(6.2)     tempFactor   Temperature  factor.
+     77 - 78        LString(2)    element      Element symbol, right-justified.
+     79 - 80        LString(2)    charge       Charge  on the atom.
+
+
                                      x         y       z           score
+     0         1         2         3         4         5         6
      ...
      ATOM     16  C3  GHE     6      43.740   4.703  81.152    2   0.70
      ATOM     17  C3  PAS     7      53.154  -5.672  85.079    2   1.34
      TER
-     ATOM      1  C3  MPT     1      54.978  -6.722  63.250    3   7.13
-     ATOM      2  C3  MPT     2      67.185  -9.621  63.264    5   5.15
-     ATOM      3  C3  MPT     3      43.850  -1.771  73.576    2   3.58
-     ATOM      4  C3  MPT     4      71.490   2.115  69.358    3   2.76
-     ATOM      5  C3  MPT     5      62.504 -20.316  65.450    1   2.53
-     ATOM      6  C3  MPT     6      43.755   4.354  81.911    2   1.69
-     ATOM      7  C3  MPT     7      53.154  -5.672  85.079    1   1.34
+     ATOM      1  C3  MPT     1     -25.464  69.313-132.566    6  15.41
+     ATOM      2  C3  MPT     2     -14.482  55.768-102.619    5   9.13
+     ATOM      3  C3  MPT     3     -28.586  58.568 -94.116    1   2.89
+     ATOM      4  C3  MPT     4     -21.459  52.766-115.823    2   1.91
+     ATOM      5  C3  MPT     5     -19.953  62.558-115.965    1   1.23
+     ATOM      6  C3  MPT     6     -22.499  67.767 -92.176    1   1.11
+     ATOM      7  C3  MPT     7     -24.911  52.100 -91.121    1   0.83
+
      */
     List<MetaPocket2Pocket> loadSitehoundPockets(String predictionOutputFile, Protein liganatedProtein) {
 
@@ -63,13 +86,13 @@ class MetaPocket2Loader extends PredictionLoader {
 
             MetaPocket2Pocket poc = new MetaPocket2Pocket()
 
-            poc.rank = cols[4].toInteger()
+            poc.rank = cols[1].toInteger()
             poc.name =  "pocket" + poc.rank
-            poc.score = cols[9].toDouble()
+            poc.score = line.substring(60, 65).toDouble()
 
-            double x = cols[5].toDouble()
-            double y = cols[6].toDouble()
-            double z = cols[7].toDouble()
+            double x = line.substring(30, 37).toDouble()
+            double y = line.substring(38, 45).toDouble()
+            double z = line.substring(46, 53).toDouble()
 
             poc.centroid = new Point(x, y, z)
             if (liganatedProtein!=null) {
