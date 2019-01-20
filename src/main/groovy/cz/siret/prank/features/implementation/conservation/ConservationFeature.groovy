50c96e393f38c0b5e9e6b060bbf0257b0d863b21
rdk <rdk@users.noreply.github.com>
2019-01-20 12:18:33 +0100

    conservation fix

--- src/main/groovy/cz/siret/prank/features/implementation/conservation/ConservationFeature.groovy
+++ src/main/groovy/cz/siret/prank/features/implementation/conservation/ConservationFeature.groovy
@@ -5,6 +5,7 @@ import cz.siret.prank.features.api.AtomFeatureCalculationContext
 import cz.siret.prank.features.api.AtomFeatureCalculator
 import cz.siret.prank.features.api.ProcessedItemContext
 import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
 import org.biojava.nbio.structure.Atom
 import org.biojava.nbio.structure.Group
 import org.biojava.nbio.structure.GroupType
@@ -12,6 +13,7 @@ import org.biojava.nbio.structure.GroupType
 /**
  * Simple single value Conservation feature that adds conservation from evolution from HSSP database or conservation pipeline to Atom feature vector.
  */
+@Slf4j
 @CompileStatic
 class ConservationFeature extends AtomFeatureCalculator {
 
@@ -24,21 +26,23 @@ class ConservationFeature extends AtomFeatureCalculator {
     void preProcessProtein(Protein protein, ProcessedItemContext itemContext) {
         // Check if conservation is already loaded.
         if (!protein.secondaryData.getOrDefault(ConservationScore.CONSERV_LOADED_KEY, false)
-                && itemContext.auxData.getOrDefault(ConservationScore.CONSERV_SCORE_KEY,
-                null) != null) {
+                && itemContext.auxData.getOrDefault(ConservationScore.CONSERV_PATH_FUNCTION_KEY, null) != null) {
             // Load conservation score.
             protein.loadConservationScores(itemContext)
+        }
+        if (protein.secondaryData.get(ConservationScore.CONSERV_SCORE_KEY) == null) {
+            log.warn "Conservation not loaded for [{}]", protein.name
         }
     }
 
     @Override
-    double[] calculateForAtom(Atom proteinSurfaceAtom, AtomFeatureCalculationContext protein) {
+    double[] calculateForAtom(Atom proteinSurfaceAtom, AtomFeatureCalculationContext context) {
         Group parentAA = proteinSurfaceAtom.getGroup()
         if (parentAA.getType() != GroupType.AMINOACID) {
             return [0.0] as double[]
         }
 
-        ConservationScore score = (ConservationScore) protein.protein.secondaryData.get(ConservationScore.CONSERV_SCORE_KEY)
+        ConservationScore score = (ConservationScore) context.protein.secondaryData.get(ConservationScore.CONSERV_SCORE_KEY)
         if (score == null) {
             return [0.0] as double[]
         } else {
