9a949b8329bf4b13fea9b1db72caa8a259d57ce6
rdk <rdk@users.noreply.github.com>
2020-02-24 01:20:59 +0100

    ions config update

--- config/ions-rdk.groovy
+++ config/ions-rdk.groovy
@@ -6,13 +6,13 @@ import cz.siret.prank.program.params.Params
      * define this if you want dataset program parameters to be evaluated relative to this directory
      * (set absolute path or path relative to install dir, null defaults to working dir)
      */
-    dataset_base_dir = "../../p2rank-data-ions/"
+    dataset_base_dir = "../../p2rank-ions-data-rdk/"
 
     /**
      * all output of the program will be stored in subdirectores of this directory
      * (set absolute path or path relative to install dir, null defaults to working dir)
      */
-    output_base_dir = "../../p2rank-results-ions/"
+    output_base_dir = "../../p2rank-ions-results/"
 
     predict_residues = false
 
