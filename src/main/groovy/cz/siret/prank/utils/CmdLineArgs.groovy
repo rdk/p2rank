49d4aa31551093cc537cf02f1a031fcb257221c3
rdk <rdk@users.noreply.github.com>
2020-03-24 11:49:35 +0100

    fixed groovy regression

--- src/main/groovy/cz/siret/prank/utils/CmdLineArgs.groovy
+++ src/main/groovy/cz/siret/prank/utils/CmdLineArgs.groovy
@@ -46,7 +46,7 @@ class CmdLineArgs {
             String arg = args[i]
             String next = (i+1<args.length) ? args[i+1] : null
 
-            if (this.isArgName(arg)) {
+            if (isArgName(arg)) {
                 String argName = stripArgName(arg)
                 if (next==null || isArgName(next) ) {
                     switches.add(argName)
@@ -63,13 +63,15 @@ class CmdLineArgs {
         }
     }
 
-    protected String stripArgName(String arg) {
+    // private (Groovy 3 has a problem with it)
+    String stripArgName(String arg) {
         if (arg.startsWith("--")) return arg.substring(2)
         if (arg.startsWith("-")) return arg.substring(1)
         return arg
     }
 
-    protected boolean isArgName(String arg) {
+    // private (Groovy 3 has a problem with it)
+    boolean isArgName(String arg) {
         if (arg==null) return false
         return arg.startsWith("-") || arg.startsWith("--")
     }
