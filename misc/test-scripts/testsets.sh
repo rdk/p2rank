bf1c83d20b570e6b44c4bcdb1b38bbbd5e86476e
rdk <rdk@users.noreply.github.com>
2020-11-01 20:58:33 +0100

    update test script

--- misc/test-scripts/testsets.sh
+++ misc/test-scripts/testsets.sh
@@ -129,9 +129,9 @@ conservation() {
 
    title PREDICTIONS USING CONSERVATION
 
-   test ./prank.sh predict joined.ds   -c distro/config/conservation -dataset_base_dir '../../p2rank-datasets' -da -conservation_dir 'joined/conservation/e5i1/scores'   -fail_fast 1 -log_cases 1 -visualizations 0 -out_subdir TEST/PREDICT_CONSERV
-   test ./prank.sh predict coach420.ds -c distro/config/conservation -dataset_base_dir '../../p2rank-datasets' -da -conservation_dir 'coach420/conservation/e5i1/scores' -fail_fast 1 -log_cases 1 -visualizations 0 -out_subdir TEST/PREDICT_CONSERV
-   test ./prank.sh predict holo4k.ds   -c distro/config/conservation -dataset_base_dir '../../p2rank-datasets' -da -conservation_dir 'holo4k/conservation/e5i1/scores'   -fail_fast 1 -log_cases 1 -visualizations 0 -out_subdir TEST/PREDICT_CONSERV
+   test ./prank.sh predict joined.ds   -c distro/config/conservation -dataset_base_dir '../../p2rank-datasets' -conservation_dir 'joined/conservation/e5i1/scores'   -fail_fast 1 -log_cases 1 -visualizations 0 -out_subdir TEST/PREDICT_CONSERV
+   test ./prank.sh predict coach420.ds -c distro/config/conservation -dataset_base_dir '../../p2rank-datasets' -conservation_dir 'coach420/conservation/e5i1/scores' -fail_fast 1 -log_cases 1 -visualizations 0 -out_subdir TEST/PREDICT_CONSERV
+   test ./prank.sh predict holo4k.ds   -c distro/config/conservation -dataset_base_dir '../../p2rank-datasets' -conservation_dir 'holo4k/conservation/e5i1/scores'   -fail_fast 1 -log_cases 1 -visualizations 0 -out_subdir TEST/PREDICT_CONSERV
 
 }
 
