0d8d0cdbecebf1c6b2bb75aed2188c72f2b09480
rdk <rdk@users.noreply.github.com>
2020-11-24 23:29:27 +0100

    re-enable score histograms

--- src/main/groovy/cz/siret/prank/program/routines/results/EvalResults.groovy
+++ src/main/groovy/cz/siret/prank/program/routines/results/EvalResults.groovy
@@ -3,6 +3,7 @@ package cz.siret.prank.program.routines.results
 import cz.siret.prank.domain.Dataset
 import cz.siret.prank.prediction.metrics.ClassifierStats
 import cz.siret.prank.prediction.metrics.Curves
+import cz.siret.prank.prediction.metrics.Histogram
 import cz.siret.prank.program.params.Parametrized
 import cz.siret.prank.utils.csv.CSV
 import cz.siret.prank.utils.Formatter
@@ -237,12 +238,11 @@ class EvalResults implements Parametrized, Writable  {
         String dir = "$outdir/$fileLabel"
         mkdirs(dir)
 
-        //cs.histograms.properties.findAll { it.value instanceof Histogram }.each {
-        //    String hist_label = it.key
-        //    Histogram hist = (Histogram) it.value
-        //
-        //    writeFile "$dir/hist_${hist_label}.csv", hist.toCSV()
-        //}
+        cs.histograms.properties.findAll { it.value instanceof Histogram }.each {
+            String hist_label = it.key
+            Histogram hist = (Histogram) it.value
+            writeFile "$dir/hist_${hist_label}.csv", hist.toCSV()
+        }
 
         if (cs.collecting && params.stats_curves)
             writeFile "$dir/roc_curve.csv", Curves.roc(cs.predictions).toCSV()
