24ed917e0ce1cf8cc0315b6977f0ab91396f7f98
rdk <rdk@users.noreply.github.com>
2020-12-18 07:38:03 +0100

    add predict-eval in residue mode

--- src/main/groovy/cz/siret/prank/program/routines/predict/PredictResiduesRoutine.groovy
+++ src/main/groovy/cz/siret/prank/program/routines/predict/PredictResiduesRoutine.groovy
@@ -14,7 +14,10 @@ import cz.siret.prank.prediction.transformation.ScoreTransformer
 import cz.siret.prank.program.ml.Model
 import cz.siret.prank.program.rendering.OldPymolRenderer
 import cz.siret.prank.program.routines.Routine
+import cz.siret.prank.program.routines.results.EvalResults
 import cz.siret.prank.program.routines.results.PredictResults
+import cz.siret.prank.program.routines.traineval.EvalResiduesRoutine
+import cz.siret.prank.program.routines.traineval.EvalRoutine
 import cz.siret.prank.utils.Futils
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
@@ -57,7 +60,8 @@ class PredictResiduesRoutine extends Routine {
     Dataset.Result execute() {
         def timer = startTimer()
 
-        write "predicting residues for proteins from dataset [$dataset.name]"
+        write "predicting and evaluating residues for proteins from dataset [$dataset.name]"
+
 
         if (produceFilesystemOutput) {
             mkdirs(outdir)
@@ -79,91 +83,98 @@ class PredictResiduesRoutine extends Routine {
             mkdirs(predDir)
         }
 
-        PredictResults stats = new PredictResults()
-        FeatureExtractor extractor = FeatureExtractor.createFactory()
 
-        if (!collectStats) {
-            LoaderParams.ignoreLigandsSwitch = true
-        }
+        EvalResiduesRoutine evalRoutine = new EvalResiduesRoutine(dataset, model, outdir)
+        EvalResults res = evalRoutine.execute()
+        
+        return res.datasetResult
 
-        boolean outputPredictionFiles = produceFilesystemOutput && !params.output_only_stats
-
-        Dataset.Result result = dataset.processItems { Dataset.Item item ->
-
-            PredictionPair pair = item.predictionPair
-            PocketRescorer rescorer = new ModelBasedRescorer(model, extractor)
-            rescorer.reorderPockets(pair.prediction, item.getContext()) // in this context reorderPockets() makes predictions
-
-            if (produceVisualizations) {
-                new OldPymolRenderer(visDir).visualizeHistograms(item, rescorer, pair)
-            }
-
-            if (outputPredictionFiles) {
-                PredictionSummary psum = new PredictionSummary(pair.prediction)
-                String outf = "$predDir/${item.label}_predictions.csv"
-                writeFile(outf, psum.toCSV().toString())
-
-                if (params.label_residues && pair.prediction.residueLabelings!=null) {
-                    String resf = "$predDir/${item.label}_residues.csv"
-                    writeFile(resf, pair.prediction.residueLabelings.toCSV())
-                }
-            }
-
-            // do eval
-            if (collectStats) {  // expects dataset with liganated proteins
-
-                // add observed binary labeling for residues (only in eval-predict)
-                if (params.label_residues && pair.prediction.residueLabelings!=null) {
-                    BinaryLabeling observed = new LigandBasedResidueLabeler().getBinaryLabeling(pair.protein)
-                    pair.prediction.residueLabelings.observed = observed
-                }
-
-                stats.evaluation.addPrediction(pair, pair.prediction.pockets)
-                synchronized (stats.classStats) {
-                    stats.classStats.addAll(rescorer.stats)
-                }
-            }
-
-            if (!dataset.cached) {
-                item.cachedPair = null
-            }
-        }
-
-        // stats and score transformer training
-        if (collectStats && produceFilesystemOutput) {
-            String modelLabel = model.classifier.class.simpleName + " ($modelf)"
-            stats.logAndStore(outdir, modelLabel)
-            stats.logMainResults(outdir, modelLabel)
-
-            // train score transformers
-            if (params.train_score_transformers != null) {
-                String scoreDir = "$outdir/score"
-                mkdirs(scoreDir)
-                for (String name : params.train_score_transformers) {
-                    try {
-                        ScoreTransformer transformer = ScoreTransformer.create(name)
-                        transformer.trainForPockets(stats.evaluation)
-                        String fname = "$scoreDir/${name}.json"
-                        writeFile(fname, ScoreTransformer.saveToJson(transformer))
-                        write "Trained score transformer '$name' written to: $fname"
-                    } catch (Exception e) {
-                        log.error("Failed to train score transformer '$name'", e)
-                    }
-                }
-            }
-
-            if (params.label_residues && params.train_score_transformers_for_residues) {
-                ResidueLabelings.trainResidueScoreTransformers(outdir, stats.evaluation)
-            }
-        }
-
-        write "predicting pockets finished in $timer.formatted"
-
-        if (produceFilesystemOutput) {
-            write "results saved to directory [${Futils.absPath(outdir)}]"
-        }
-
-        return result
+//
+//        PredictResults stats = new PredictResults()
+//        FeatureExtractor extractor = FeatureExtractor.createFactory()
+//
+//        if (!collectStats) {
+//            LoaderParams.ignoreLigandsSwitch = true
+//        }
+//
+//        boolean outputPredictionFiles = produceFilesystemOutput && !params.output_only_stats
+//
+//        Dataset.Result result = dataset.processItems { Dataset.Item item ->
+//
+//            PredictionPair pair = item.predictionPair
+//            PocketRescorer rescorer = new ModelBasedRescorer(model, extractor)
+//            rescorer.reorderPockets(pair.prediction, item.getContext()) // in this context reorderPockets() makes predictions
+//
+//            if (produceVisualizations) {
+//                new OldPymolRenderer(visDir).visualizeHistograms(item, rescorer, pair)
+//            }
+//
+//            if (outputPredictionFiles) {
+//                PredictionSummary psum = new PredictionSummary(pair.prediction)
+//                String outf = "$predDir/${item.label}_predictions.csv"
+//                writeFile(outf, psum.toCSV().toString())
+//
+//                if (params.label_residues && pair.prediction.residueLabelings!=null) {
+//                    String resf = "$predDir/${item.label}_residues.csv"
+//                    writeFile(resf, pair.prediction.residueLabelings.toCSV())
+//                }
+//            }
+//
+//            // do eval
+//            if (collectStats) {  // expects dataset with liganated proteins
+//
+//                // add observed binary labeling for residues (only in eval-predict)
+//                if (params.label_residues && pair.prediction.residueLabelings!=null) {
+//                    BinaryLabeling observed = new LigandBasedResidueLabeler().getBinaryLabeling(pair.protein)
+//                    pair.prediction.residueLabelings.observed = observed
+//                }
+//
+//                stats.evaluation.addPrediction(pair, pair.prediction.pockets)
+//                synchronized (stats.classStats) {
+//                    stats.classStats.addAll(rescorer.stats)
+//                }
+//            }
+//
+//            if (!dataset.cached) {
+//                item.cachedPair = null
+//            }
+//        }
+//
+//        // stats and score transformer training
+//        if (collectStats && produceFilesystemOutput) {
+//            String modelLabel = model.classifier.class.simpleName + " ($modelf)"
+//            stats.logAndStore(outdir, modelLabel)
+//            stats.logMainResults(outdir, modelLabel)
+//
+//            // train score transformers
+//            if (params.train_score_transformers != null) {
+//                String scoreDir = "$outdir/score"
+//                mkdirs(scoreDir)
+//                for (String name : params.train_score_transformers) {
+//                    try {
+//                        ScoreTransformer transformer = ScoreTransformer.create(name)
+//                        transformer.trainForPockets(stats.evaluation)
+//                        String fname = "$scoreDir/${name}.json"
+//                        writeFile(fname, ScoreTransformer.saveToJson(transformer))
+//                        write "Trained score transformer '$name' written to: $fname"
+//                    } catch (Exception e) {
+//                        log.error("Failed to train score transformer '$name'", e)
+//                    }
+//                }
+//            }
+//
+//            if (params.label_residues && params.train_score_transformers_for_residues) {
+//                ResidueLabelings.trainResidueScoreTransformers(outdir, stats.evaluation)
+//            }
+//        }
+//
+//        write "predicting pockets finished in $timer.formatted"
+//
+//        if (produceFilesystemOutput) {
+//            write "results saved to directory [${Futils.absPath(outdir)}]"
+//        }
+//
+//        return result
     }
 
 }
